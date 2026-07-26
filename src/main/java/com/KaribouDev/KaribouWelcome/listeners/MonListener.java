9a9f3a411cd9437ea7f0429ffe606934d9f3d22f
oli <217653579+KaribouDev@users.noreply.github.com>
2026-07-26 09:18:10 -0400

    feat: adding a quit custome message for te deconnection of a player

--- src/main/java/com/KaribouDev/KaribouWelcome/listeners/MonListener.java
+++ src/main/java/com/KaribouDev/KaribouWelcome/listeners/MonListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.KaribouDev.KaribouWelcome.MonPlugin;
 
@@ -20,13 +21,23 @@ public class MonListener implements Listener {
         Player player = event.getPlayer();
 
         if (!player.hasPlayedBefore()) {
+            // Custom join message for new players
             event.joinMessage(plugin.getConfigMessage(player, "public.new-player"));
 
+            // Send welcome and rules messages to new players
             plugin.sendConfigMessages(player, "private.welcome");
             plugin.sendConfigMessages(player, "private.rules");
 
         } else {
+            // Custom join message for returning players
             event.joinMessage(plugin.getConfigMessage(player, "public.login"));
         }
+    }
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        
+        // Custom quit message
+        event.quitMessage(plugin.getConfigMessage(player, "public.quit"));
     }
 }
